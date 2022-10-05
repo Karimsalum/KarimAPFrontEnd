4a5b09dab0c81529b61b41b35570d074eba1ef91
Karimsalum <karimrsalum@gmail.com>
2022-10-05 14:08:00 -0300

    prueba

--- src/app/service/skill.service.ts
+++ src/app/service/skill.service.ts
@@ -2,14 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { identifierName } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+//import { environment } from 'src/environments/environment';
 import { Skill } from '../model/skill';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SkillService {
-  URL = environment.URL + 'skill/';
+  URL = 'https://karasaapp.herokuapp.com/' + 'skill/';
   
   constructor(private httpClient: HttpClient) { }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const environment = {
- production: true,
- URL: 'https://karasaapp.herokuapp.com/'
-};
+//export const environment = {
+ // production: true,
+ // URL: 'https://karasaapp.herokuapp.com/'
+//};
