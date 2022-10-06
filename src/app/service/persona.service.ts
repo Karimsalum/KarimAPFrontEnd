47c824ef91df48d6e19758f57f5bf3e9fc02ab0d
Karimsalum <karimrsalum@gmail.com>
2022-10-05 23:43:41 -0300

    a ver si anda

--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+//import { environment } from 'src/environments/environment';
 import { persona } from '../model/persona.model';
 
 @Injectable({
@@ -9,7 +9,7 @@ import { persona } from '../model/persona.model';
 })
 
 export class PersonaService {
-  URL = environment.URL + 'personas/';
+  URL = 'https://karasaapp.herokuapp.com/' + 'personas/';
 
   constructor(private http: HttpClient) { }
 
