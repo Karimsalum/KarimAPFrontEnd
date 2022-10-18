e1d5276e656563e2f4e5e15ed45ff24de8dfbf40
Karimsalum <karimrsalum@gmail.com>
2022-10-18 13:53:40 -0300

    fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'karimfrontapp',
+    appId: '1:1058808347874:web:d830691d4e4867984032bb',
+    storageBucket: 'karimfrontapp.appspot.com',
+    apiKey: 'AIzaSyANo42dG5yQao2Bq0LmKJFkLj9vVgo3J-8',
+    authDomain: 'karimfrontapp.firebaseapp.com',
+    messagingSenderId: '1058808347874',
+  },
  production: true,
  URL: 'https://karasaapp.herokuapp.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'karimfrontapp',
+    appId: '1:1058808347874:web:d830691d4e4867984032bb',
+    storageBucket: 'karimfrontapp.appspot.com',
+    apiKey: 'AIzaSyANo42dG5yQao2Bq0LmKJFkLj9vVgo3J-8',
+    authDomain: 'karimfrontapp.firebaseapp.com',
+    messagingSenderId: '1058808347874',
+  },
 
 production: false,
   URL: 'http://localhost:8080/'
